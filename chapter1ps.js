d99c1944f822c9852c2cc898f6d3ce4470974c78
Raghav2124 <raghav.aggarwal@testingxperts.com>
2024-05-31 16:46:16 +0530

    chapter 1

---
+++ chapter1ps.js
@@ -0,0 +1,16 @@
+// chapter 1 q1
+let a="raghav"
+let b=2
+let c=a+b
+console.log(a+b)
+//q2
+console.log(typeof(c))
+//q3
+const d={
+    name:'raghav',
+    section:"10d"
+}
+d=54//this will give an error 
+//q4
+d["h"]="yo"//this will not give an error 
+//q5 really boring
---
+++ primitive.js
@@ -0,0 +1,27 @@
+//nn ss bb u 
+//primitive data type 
+let a=4;
+let b=null
+let c="raghav"
+let d=symbol("I am a symbol")
+let e=true 
+let f=BigInt("567")+BigInt("3")
+let g=undefined
+console.log(a)
+console.log(b)
+
+console.log(c)
+console.log(d)
+console.log(e)
+console.log(f)
+console.log(a)
+console.log(typeof(c))
+//non primitive data type 
+
+const item={
+    "raghav":true,
+    "shubh":false,
+    "lovish":9,
+    "rohan":undefined
+}
+console.log(item["shubh"])
