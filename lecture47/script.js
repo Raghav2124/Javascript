4e099f25a0d13e5df3b33661ea5c0f3c8ecf8fa6
RaghavAggarwal10 <aggarwal.raghav170@gmail.com>
2024-06-15 17:03:41 +0530

    events

--- lecture47/script.js
+++ lecture47/script.js
@@ -1,2 +1,7 @@
 //  an event is basically a signal that something has happened 
 //also whatever we do on browser we are creating a event 
+let a = document.getElementsByClassName("container")[0]
+a.onclick = () => {
+  let b = document.getElementsByClassName("container")[0]
+  b.innerHTML = "Hello World!"
+}
