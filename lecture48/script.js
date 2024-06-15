20dc88274342994d0b7e6c58b78222a9145b30bd
RaghavAggarwal10 <aggarwal.raghav170@gmail.com>
2024-06-16 02:03:02 +0530

    addevent listeners

---
+++ lecture48/script.js
@@ -0,0 +1,7 @@
+//add event listeners  is used to assign multiple handlers to an event 
+btn.addEventListener('click',function(e){
+    alert('Hello world!1')
+})
+btn.addEventListener('click',function(e){
+    alert('Hello world!2')
+})
