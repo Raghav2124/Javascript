addf2af394c41690596fb2daa4295cc02edf1cc0
RaghavAggarwal10 <aggarwal.raghav170@gmail.com>
2024-06-15 14:40:27 +0530

    adding files

---
+++ lecture44/script.js
@@ -0,0 +1,6 @@
+first.insertAdjacentHtml("beforeend",'<div class="test">beforeend</div>')//this will insert just before the div take is closed
+first.insertAdjacentHtml("beforebegin",'<div class="test">beforebegin</div>')//this will insert just before the div take is placed there
+first.insertAdjacentHtml("aftrebegin",'<div class="test">afterbegin</div>')//this will insert the element just after the div tag is opened
+first.insertAdjacentHtml("afterend",'<div class="test">afterend</div>')//this will insert the element just after it is closed 
+//to remove a node we can use .remove() function to do this 
+//if we do first.remove() the whole first is removed 
