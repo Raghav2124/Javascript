08b7a88e71c397b532c5f43d3fae5b9346c7dae7
RaghavAggarwal10 <aggarwal.raghav170@gmail.com>
2024-06-15 16:36:07 +0530

    setinterval and settimeout

---
+++ lecture45/script.js
@@ -0,0 +1,9 @@
+first.className="yellow text-black"
+// classList it constains the list of all the classes we have 
+//  we can remove a class from it by using first.classlist.remove("name of class which we want to remove ")
+// to add a class we use first.classlist.add("to add the class which we want ")
+// we can use toggle method if we want it can remove a class or add  a class like if we don't have a class it will add it and vice a versa
+first.classList.add("red")
+first.classList.remove("red")
+first.classList.contains("red")
+first.classList.toggle("red")
---
+++ lecture46/script.js
@@ -0,0 +1,17 @@
+//settimeout is used when we want to execute our javascript after some interval of time 
+//setinterval it is used when we want to execute our javascript again and again after a set interval of time 
+alert("hello")
+const sum=(a,b)=>{
+    console.log("Yes I am running " +(a+b))
+}
+setTimeout(sum,5000,1,2);// here 1 and 2 are arguments which we pass and it returns timmer id 
+let a=setTimeout(function (){
+alert("Hi i am within settimeout")
+},2000)// here 2000 is in milisecond and it is equivalent to 2 seconds 
+//in console 1 will be displayed and it tells us that it is a timer id 
+let b=prompt("Do you want to clear settimeout(y/n)")
+if("n"==b){
+clearTimeout(a)
+}//this will stop the execution of teh above timeout and this will not show any result after 2 seconds
+console.log(a)
+//setinterval has a similar syntax to settimeout but it is execucted again and again after the specified time 
