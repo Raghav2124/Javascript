c23ad23f6ea5e59bba3c59fdcdec6a4cf1173970
RaghavAggarwal10 <aggarwal.raghav170@gmail.com>
2024-06-16 23:55:45 +0530

    try and catch

--- lecture61/script.js
+++ lecture61/script.js
@@ -1,8 +1,35 @@
-try{
-    harry
+try {
+    let age = prompt("Enter your age")
+    age = Number.parseInt(age) 
+    if(age>150){
+    throw new ReferenceError("This is probably not true")
+    }
+} catch (error) {
+    console.log(error.name)
+    console.log(error.message)
+    console.log(error.stack)
 }
-catch(err){
-    console.log(err.name)    
-    console.log(err.message)    
 
+console.log("The script is still running")
+const f = () => {
+    try {
+            let a = 0;
+            // console.log(program)
+            console.log("Program ran successfully")
+            return
+    }
+    catch (err) {
+            console.log("This is an error")
+            console.log(p)
+    }
+    finally {
+            console.log("I am a good boy")
+            // Close the file
+            // Exit the Loop
+            // Write to the log file
+            //this still runs even if there is a return statement 
+    }
 }
+
+f()
+console.log("End")
